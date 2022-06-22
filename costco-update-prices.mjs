bd1d9e533b8d7fd53e19fb62f7571216c782a601
gomflo <gomflo@gmail.com>
2022-06-21 22:35:59 -0500

    Add iMacs and Macbooks category that does not exitst in the xml category file.

--- costco-update-prices.mjs
+++ costco-update-prices.mjs
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 import { formatISO9075 } from "date-fns";
 import fetch from "node-fetch";
 import xml2js from "xml2js";
-import 'dotenv/config'
+import "dotenv/config";
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
@@ -23,6 +23,8 @@ async function getCategories() {
   const urlset = doc.urlset.url;
 
   const categories = [];
+  categories.push("cos_1.5.6"); // macbooks & imacs category that does not exists in the xml file.
+
   for (let url of urlset) {
     const category = url.loc[0].split("/").pop();
     categories.push(category);
